5526cb4c7497fbab72181196d6bcd0fd398922aa
Nomi Nonsense <65523690+nomi-nonsz@users.noreply.github.com>
2024-11-28 09:31:20 +0700

    Settings responsive (#4)
    * settings responsive

--- src/app/(authorized)/settings/page.tsx
+++ src/app/(authorized)/settings/page.tsx
@@ -4,8 +4,8 @@ import { SettingsClient } from "./client"
 export default function NotesPage () {
   return (
     <main className="h-full flex flex-col gap-7">
-      <header className="my-16 text-center">
-        <h1 className={`text-4xl font-bold ${poppins.className}`}>Settings</h1>
+      <header className="sm:my-16 mb-2 mt-8 text-center">
+        <h1 className={`sm:text-4xl text-2xl font-bold ${poppins.className}`}>Settings</h1>
       </header>
       <SettingsClient />
     </main>
--- src/components/ui/settings-card.tsx
+++ src/components/ui/settings-card.tsx
@@ -1,18 +1,20 @@
 'use client'
 
+import { LucideIcon } from "lucide-react"
+
 export const SettingsCard = ({
   title,
   children,
   icon: Icon,
 }: {
   title: string,
-  icon: React.ElementType,
+  icon: LucideIcon,
   children: React.ReactNode
 }) => (
   <div className="bg-background border rounded-xl">
     <header className="p-4 flex gap-2 items-center">
-      <Icon />
-      <h2 className="font-semibold text-lg">{title}</h2>
+      <Icon className="sm:size-auto size-5" />
+      <h2 className="font-semibold sm:text-lg">{title}</h2>
     </header>
     <hr className="border-border" />
     <div className="p-4">
